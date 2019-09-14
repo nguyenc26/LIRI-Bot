bd39af2ac86a25406acc9a8e2fd961d47d6dc90a
Cuong Desktop <nguyenc72694@gmail.com>
2019-09-13 20:40:37 -0500

    started the liri.js

--- liri.js
+++ liri.js
@@ -2,5 +2,23 @@ require("dotenv").config();
 
 var keys = require("./keys.js");
 
+var Spotify = require ('node-spotify-api');
+var axios = require ('axios');
+var moment = require ('moment');
 
 var spotify = new Spotify(keys.spotify);
+
+var fs = require('fs');
+
+var nodeArgs = process.argv;
+console.log(nodeArgs)
+var userInput = "";
+
+for (var i = 3; i < nodeArgs.length; i++) {
+    
+    if (i > 3 && i < nodeArgs.length) {
+        userInput = userInput + "+" + nodeArgs[i];
+    } else {
+        userInput += nodeArgs[i];
+    };
+};
